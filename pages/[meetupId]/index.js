080615577567140a1aaea3338eebf4f58b376b4c
AshokYadav <yashoka51@gmail.com>
2024-03-28 02:20:10 +0530

    detail page

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Card from '../../components/ui/Card'
 import MeetupDetail from '../../components/meetups/MeetupDetail'
+import { MongoClient, ObjectId } from 'mongodb'
+
 const DetailPage = (props) => {
   console.log(props.meetupData.id)
   return (
@@ -15,37 +17,60 @@ const DetailPage = (props) => {
   )
 }
 export async function getStaticPaths(){
-  return {
-    fallback:false,
-    paths:[
-      {
-        params:{
-          meetupId:'m1'
-        }
-      },
-      {
-        params:{
-          meetupId:'m2'
-        }
-      },
-    ]
-  }
-}
-export async function getStaticProps(context){
-  let Id=context.params.meetupId;
-  return {
-    props:{
-      meetupData:{
-        id:Id,
-        image:'https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=600',
-        title:'just an image',
-        address:'vpo lidhran jalandhar',
-        description:'nice place'
-      }
+
+  const url='mongodb+srv://yashoka51:WUy6Be5gRxGPWV1z@cluster0.ndzb68f.mongodb.net/'
+  // 'mongodb+srv://yashoka51:WUy6Be5gRxGPWV1z@cluster0.ndzb68f.mongodb.net/meeetups?retryWrites=true&w=majority&appName=Cluster0'
+  
+  const client = await MongoClient.connect('mongodb+srv://yashoka51:WUy6Be5gRxGPWV1z@cluster0.ndzb68f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+ 
+      const db=client.db('meetups2');
+
+      const meetupsCollection=db.collection('meetupDetail2');
+
+      const meetups=await meetupsCollection.find({},{_id:1}).toArray();
+
+      // console.log(meetups)
+      client.close();
+
+  return{
+    fallback:'blocking',
+    paths:meetups.map(meetup=>({
+    params:{
+      meetupId:meetup._id.toString()
     }
+    })),
   }
 }
 
+export async function getStaticProps(context){
+  
+  const meetupid=context.params.meetupId;
 
+  const url='mongodb+srv://yashoka51:WUy6Be5gRxGPWV1z@cluster0.ndzb68f.mongodb.net/'
+  // 'mongodb+srv://yashoka51:WUy6Be5gRxGPWV1z@cluster0.ndzb68f.mongodb.net/meeetups?retryWrites=true&w=majority&appName=Cluster0'
+  
+  const client = await MongoClient.connect('mongodb+srv://yashoka51:WUy6Be5gRxGPWV1z@cluster0.ndzb68f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+ 
+      const db=client.db('meetups2');
+
+      const meetupsCollection=db.collection('meetupDetail2');
+
+      const selectedMeetup=await meetupsCollection.findOne({_id: new ObjectId(meetupid)});
+
+      // console.log(meetups)
+      client.close();
+
+  return {
+      props:{
+         meetupData:{
+          id:selectedMeetup._id.toString(),
+          title:selectedMeetup.title,
+          image:selectedMeetup.image,
+          address:selectedMeetup.address,
+          description:selectedMeetup.description
+         }
+      }
+  };
+}
 
 export default DetailPage
